457b7dfe4e2b34c9e446c444a0c491d6de535d15
Debo <dwdeante0726@gmail.com>
2023-07-01 17:44:36 -0700

    Changing logic of numberDetector function to flow with weCountRealGood function for upcoming call of each other.

--- js/scripts.js
+++ js/scripts.js
@@ -4,28 +4,37 @@ function numberDetector(text) {
 
   //Separator for look between each number occurrennce.
   const numbers = text.split(', ');
+  let result = '';
 
   //Finding the different numbers in order as shown in Epicenter ex.
   numbers.forEach(function(number, index) {
 
     //Array for looking for aany occurrrence of numbers 1-3.
-    console.log(index)
-    if (/[3]/.text(number, index)) {
-      numbers[index] = number.replace('3', "Won't you be my neighbor?");
-    } else if (number === 2) {
-      numbers[index] = number.replace('2'. "Boop!");
-    } else if (number === 1) {
-      numbers[index] = number.replace('1',"Beep!");
+    if (/[1-3]/.text(number)) {
+      result += "Won't you be my neighbor?";
+    } else if (/[1-2]/.text(number)) {
+      result += "Boop!";
+    } else if (/[1]/.text(number)) {
+      result += "Beep!";
     }
   })
   return numbers
 } 
 
+//Declaring function for counting up to an inputted number!
 function weCountRealGood(num) {
+
+  //made a string for result
   let result = '';
+
+  //This for loop tells us to go from 1 to inputted number (num).
   for (let i = 1; i <= num; i++) {
+
+    //Adds 1 to empty result string.
     result += i;
     if (i < num) {
+
+      //Replaces empty string at first, then separates each number so on adn so forth. 
       result =+ ', ';
     }
   }
