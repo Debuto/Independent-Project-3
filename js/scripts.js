b8188e523d5f61366431f9f6dbeaf1943952d54a
Debo <dwdeante0726@gmail.com>
2023-07-01 18:20:25 -0700

    This will work on adding a prevent default logic and attaching logic to html.

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,24 @@
+form.addEventListener('submit', function(event) {
+  event.preventDefault(); // Prevent the form from submitting and refreshing the page
+
+
 //Declaring function for detecting nnumbers 1, 2, and 3!
 
-function numberDetector(text) {
-
-  //Separator for look between each number occurrennce.
-  const numbers = text.split(', ');
+function numberDetector(input) {
   let result = '';
-
-  //Finding the different numbers in order as shown in Epicenter ex.
-  numbers.forEach(function(number, index) {
-
-    //Array for looking for aany occurrrence of numbers 1-3.
-    if (/[1-3]/.test(number)) {
-      result += "Won't you be my neighbor?";
-    } else if (/[1-2]/.text(number)) {
-      result += "Boop!";
-    } else if (/[1]/.text(number)) {
-      result += "Beep!";
-    }
-  })
-  return numbers
-} 
-
-//Declaring function for counting up to an inputted number!
-function weCountRealGood(num) {
-
-  //made a string for result
-  let result = '';
-
-  //This for loop tells us to go from 1 to inputted number (num).
-  for (let i = 1; i <= num; i++) {
-
-    //Adds 1 to empty result string.
-    result += i;
-    if (i < num) {
-
-      //Replaces empty string at first, then separates each number so on adn so forth. 
-      result =+ ', ';
+  for (let i = 0; i <= input; i++) {
+    if (i === 0) {
+      result += i;
+    } else if (i === 1) {
+      result += ', "Beep"';
+    } else if (i === 2) {
+      result += ', "Boop!"';
+    } else if (i === 3) {
+      result += ', "Won\'t you be my neighbor?"';
+    } else if (/[456789]/.test(i.toString())) {
+      result += ', ' + i;
+    } else if (/[01]/.test(i.toString())) {
+      result += ', "Beep!"';
     }
   }
   return result;
