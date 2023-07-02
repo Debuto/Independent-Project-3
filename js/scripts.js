a3ff54ed63088ff68fdd945750606c91af3cbfef
Debo <dwdeante0726@gmail.com>
2023-07-01 19:04:51 -0700

    This will not add anything was further testing logic...

--- js/scripts.js
+++ js/scripts.js
@@ -11,12 +11,12 @@ function numberDetector(input) {
   for (let i = 0; i <= input; i++) {
     if (i === 0) {
       result += i;
-    } else if (i === 1) {
-      result += ', "Beep"';
-    } else if (i === 2) {
-      result += ', "Boop!"';
     } else if (i === 3) {
       result += ', "Won\'t you be my neighbor?"';
+    } else if (i === 2) {
+      result += ', "Boop!"';
+    } else if (i === 1) {
+      result += ', "Beep"';
     } else if (/[456789]/.test(i.toString())) {
       result += ', ' + i;
     } else if (/[01]/.test(i.toString())) {
