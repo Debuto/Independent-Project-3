7cc9b9ffbb0b738fa65d9dc08c054184293a9316
Debo <dwdeante0726@gmail.com>
2023-07-08 21:53:10 -0700

    This will move <script> element in html from <body> element to <head> element.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,27 @@
-document.getElementById('numberForm').addEventListener('submit', function(event) {
-  event.preventDefault();
-  const numberInput = document.getElementById('number').value;
-  const outputDiv = document.getElementById('output');
-  const result = numberDetector(Number(numberInput));
-  outputDiv.innerText = result;
-});p
+window.addEventListener('load', function() {
+  document.getElementById('numberForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const numberInput = document.getElementById('number').value;
+    const outputDiv = document.getElementById('output');
+    const result = numberDetector(Number(numberInput));
+    outputDiv.innerText = result;
+  });p
 
-function numberDetector(input) {
-  let result = '';
-  for (let i = 0; i <= input; i++) {
-    if (i === 0) {
-      result += i;
-    } else if (String(i).includes('3')) {
-      result += ', "Won\'t you be my neighbor?"';
-    } else if (String(i).includes('2')) {
-      result += ', "Boop!"';
-    } else if (String(i).includes('1')) {
-      result += ', "Beep"';
+  function numberDetector(input) {
+    let result = '';
+    for (let i = 0; i <= input; i++) {
+      if (i === 0) {
+        result += i;
+      } else if (String(i).includes('3')) {
+        result += ', "Won\'t you be my neighbor?"';
+      } else if (String(i).includes('2')) {
+        result += ', "Boop!"';
+      } else if (String(i).includes('1')) {
+        result += ', "Beep"';
+      }
     }
+    return result;
   }
-  return result;
-}
+  
+})
+
