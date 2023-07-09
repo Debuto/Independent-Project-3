0f8117eb7df2732a059d5dea4bba1b75bcbb73f6
Debo <dwdeante0726@gmail.com>
2023-07-08 21:26:56 -0700

    This will update logic in scripts to use include method instead...

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ document.getElementById('numberForm').addEventListener('submit', function(event)
   const outputDiv = document.getElementById('output');
   const result = numberDetector(Number(numberInput));
   outputDiv.innerText = result;
-});
+});p
 
 //Declared funnction for detecting numbers 1-3.0
 
@@ -13,21 +13,12 @@ function numberDetector(input) {
   for (let i = 0; i <= input; i++) {
     if (i === 0) {
       result += i;
-    } else if (i === 3) {
+    } else if (String(i).includes('3')) {
       result += ', "Won\'t you be my neighbor?"';
-    } else if (i === 2) {
+    } else if (String(i).includes('2')) {
       result += ', "Boop!"';
-    } else if (i === 1) {
+    } else if (String(i).includes('1')) {
       result += ', "Beep"';
-
-    //Line here was to help fill out arguement if prrevious statements were false. Commenting this line gives closer desired output of goal. 
-
-    } else if (/[987654]/.test(i.toString())) {
-      result += ', ' + i;
-    
-  
-  } else if (/[01]/.test(i.toString())) {
-      result += ', "Beep!"';
     }
   }
   return result;
